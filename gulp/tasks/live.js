3d9f8e462deb65a3d45f33560583b8a74383a216
ach0210s ach <ach0210s ach>
2016-09-28 14:57:55 +0200

    open browser when server is started

--- gulp/tasks/live.js
+++ gulp/tasks/live.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var opn = require('opn');
 
 var server_task = function() {
 
     var server = gls.static('build', 8888);
-    server.start();
-
-    //use gulp.watch to trigger server actions(notify, start or stop) 
+    server.start();    
+    opn('http://localhost:8888');
     var build_watcher = gulp.watch('build/**/*.*', function (file) {
         server.notify.apply(server, [file]);        
     });
